1ad39f24265221ac82caa71d7474f629e2fa0d9e
riya24vaidya <riya@devloops.in>
2024-04-22 08:54:13 +0530

    riya 4th commit dev 22/04 after taing pull..assets bind of $post->hero in blg dtl

--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -6,7 +6,7 @@
         <!-- breadcrumb-area-start -->
         <section class="breadcrumb-area breadcrumb-wrap">
 
-            <div class="breadcrumb-bg" data-background="{{ $post->hero }}"></div>
+            <div class="breadcrumb-bg" data-background="{{assest($post->hero)}}"></div>
 
             <div class="container">
                 <div class="row align-items-center">
