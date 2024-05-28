99d61d6b228c7e01ed01a6048ff0033be220917e
riya24vaidya <riya@devloops.in>
2024-05-28 13:15:26 +0530

    riya 1st commit devllops 28/05
    googlge analy in head of app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -59,6 +59,16 @@
  }
 }
      </style>
+     <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MG51PDS4RE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MG51PDS4RE');
+</script>
+
    </head>
    <body>
 
